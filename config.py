e3607c3625a69ad54f2b391fe63dec0e9bb13691
~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <222322462+BtwUnnati@users.noreply.github.com>
2025-10-26 15:30:57 +0530

    Update config.py
    Signed-off-by: ~ 𝐔𝐍𝐍𝚲𝐓𝐈 /𝟑 𓆩💗𓆪𓈒 <222322462+BtwUnnati@users.noreply.github.com>

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "http://lofiBots.t.me")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SxNoii")
 
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
         self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.START_IMG = getenv("START_IMG", "")
         self.VIDEO_PLAY: bool = getenv("VIDEO_PLAY", True)
 
     def check(self):
